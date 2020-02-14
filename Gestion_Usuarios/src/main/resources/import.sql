0b2de405431ce0b6f86eff2d19625f38301585bb
YohanJavierMamani <ymamani@exact.com.pe>
2020-02-14 14:58:34 -0500

    cambios oficiales

--- Gestion_Usuarios/src/main/resources/import.sql
+++ Gestion_Usuarios/src/main/resources/import.sql
@@ -1,99 +1,32 @@
-insert into permisos (nombre, descripcion) values ('ROLE_CREADOR_DE_PERFILES','CREAR PERFILES DE USUARIO')
-insert into permisos (nombre, descripcion) values ('ROLE_MODIFICADOR_DE_PERFILES','MODIFICAR PERFILES DE USUARIO')
-insert into permisos (nombre, descripcion) values ('ROLE_MODIFICADOR_DE_ESTADO_DE_PERFILES','MODIFICA EL ESTADO DE LOS PERFILES')
-insert into permisos (nombre, descripcion) values ('ROLE_ASIGNADOR_DE_PERFILES','ASIGNAR PERFILES A UN USUARIO')
-insert into permisos (nombre, descripcion) values ('ROLE_CREADOR_DE_PERMISOS','CREAR PERMISOS DE UN USUARIO')
-insert into permisos (nombre, descripcion) values ('ROLE_MODIFICADOR_DE_PERMISOS','MODIFICAR PERMISOS DE USUARIO')
-insert into permisos (nombre, descripcion) values ('ROLE_ASIGNADOR_DE_PERMISOS','ASIGNAR PERMISOS A UN PERFIL')
-insert into permisos (nombre, descripcion) values ('ROLE_QUITAR_PERMISOS','QUITAR PERMISOS A UN PERFIL')
-insert into permisos (nombre, descripcion) values ('ROLE_CREAR_USUARIOS','CREA USUARIOS CON UN PERFIL')
-insert into permisos (nombre, descripcion) values ('ROLE_MODIFICADOR_DE_ESTADO_DE_USUARIOS','MODIFICA EL ESTADO')
-insert into permisos (nombre, descripcion) values ('ROLE_MODIFICADOR_USUARIOS','MODIFICA LOS DATOS DEL USUARIO')
-insert into permisos (nombre, descripcion) values ('ROLE_USUARIO_REGULAR','USUARIO REGULAR')
-insert into permisos (nombre, descripcion) values ('ROLE_UTD','GESTIONA DOCUMENTO')
-insert into permisos (nombre, descripcion) values ('ROLE_SUPERVISOR','SUPERVISA LAS OPERACIONES DE UTD')
-insert into permisos (nombre, descripcion) values ('ROLE_AUTORIZADOR','AUTORIZA ENVIO DE DOCUMENTOS')
-insert into permisos (nombre, descripcion) values ('ROLE_USUARIO_BLOQUE','USUARIO BLOQUE')
-insert into permisos (nombre, descripcion) values ('ROLE_PROVEEDOR','PROVEEDOR')
-
-
---debe estar por defecto activo a '1'11
-insert into perfiles values (1,'ESTA A CARGO DE UN AREA ASIGNADA',GETDATE(),'JEFE')
-insert into perfiles values (1,'ES EL RESPONSABLE DEL CONTROL DE ACTIVIDADES DIARIAS Y MANEJO DE LAS OPERACIONES',GETDATE(),'SUPERVISOR')
-insert into perfiles values (1,'ES EL ENCARGADO DE REALIZAR LAS ACTIVIDADES DIARIAS DE LA EMPRESA',GETDATE(),'OPERATIVO')
-insert into perfiles values (1,'ES EL ENCARGADO DE GUARDAR TARJETAS DE CREDITO',GETDATE(),'ASISTENTE')
-insert into perfiles values (1,'CREA DOCUMENTOS DE ENVIO REGULAR',GETDATE(),'USUARIO_REGULAR')
-insert into perfiles values (1,'CONTROLA DOCUMENTOS',GETDATE(),'GESTION DOCUMENTAL')
-insert into perfiles values (1,'CREA DOCUMENTOS DE ENVIO BLOQUE',GETDATE(),'USUARIO_BLOQUE')
-insert into perfiles values (1,'PROCESAR LAS GUIAS ENVIADAS',GETDATE(),'USUARIO_PROVEEDOR')
-
---debe estar por defecto activo a '1'
---
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'ORLANDO','HEREDIA',12345698,'OHEREDIA','oheredia@exact.com.pe','oheredia','$2a$04$GKk2hQxmmUAyVqGQM3I7R.6Ua9av7/7XNz4kDE8ODzmVOFDnTxuEK')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'CHRISTIAN','CAMPOS',75435710,'CCAMPOS','ccampos@exact.com.pe','ccampos','$2a$04$NvvJE0k5kV2g3gIMxzLa0ungVBERTwH3psJ57SbFK51qDBbbAfM7q')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'RONALD','SANTOS',46059112,'RSANTOS','rsantos@exact.com.pe','rsantos','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'CESAR','BALTAZAR',58963258,'CBALTAZAR','cbaltazar@exact.com.pe','cbaltazar','$2a$04$NvvJE0k5kV2g3gIMxzLa0ungVBERTwH3psJ57SbFK51qDBbbAfM7q')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'KATHERINE','VEGA',79461382,'KVEGA','kvega@exact.com.pe','kvega','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'KATHELEEN','MACEDO',23434322,'KMACEDO','kmacedo@exact.com.pe','kmacedo','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'KRYSTEL','ARRUE',53684524,'KARRUE','karrue@exact.com.pe','karrue','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'MEDALIT','CEVALLOS',95785632,'MCEVALLOS','mcevallos@exact.com.pe','mcevallos','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'ERNST','ROJAS',79461389,'EROJAS','ccampos@exact.com.pe','erojas','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'GIOVANNA','VEGA',12354677,'GVEGA','rsantos@exact.com.pe','gvega','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'AMANDA','GOMEZ',12954627,'AGOMEZ','agomez@exact.com.pe','agomez','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'KELLY','SALAZAR',82954621,'KSALAZAR','ksalazar@exact.com.pe','ksalazar','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-insert into usuarios (activo, nombre, apellido, dni, matricula, correo, username, password) values (1,'PROVEEDOR','PROVEEDOR1',82984621,'PROVEEDOR','proveedor@exact.com.pe','proveedor','$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO')
-
-
-
-
-insert into perfiles_permisos values (1,1)
-insert into perfiles_permisos values (1,9)
-insert into perfiles_permisos values (1,10)
---insert into perfiles_permisos values (2,2)
---insert into perfiles_permisos values (2,3)
---insert into perfiles_permisos values (2,4)
---insert into perfiles_permisos values (2,5)
---insert into perfiles_permisos values (2,6)
---insert into perfiles_permisos values (2,7)
---insert into perfiles_permisos values (2,8)
---insert into perfiles_permisos values (3,5)
---insert into perfiles_permisos values (3,6)
---insert into perfiles_permisos values (3,7)
-insert into perfiles_permisos values (4,6)
-insert into perfiles_permisos values (3,13)
-insert into perfiles_permisos values (5,12)
-insert into perfiles_permisos values (2,14)
-insert into perfiles_permisos values (6,15)
-insert into perfiles_permisos values (7,16)
-insert into perfiles_permisos values (8,17)
-
-insert into usuario_perfil values (1,GETDATE(),'',3,1)
-insert into usuario_perfil values (1,GETDATE(),'',3,2)
-insert into usuario_perfil values (1,GETDATE(),'',3,3)
-insert into usuario_perfil values (1,GETDATE(),'',3,4)
-insert into usuario_perfil values (1,GETDATE(),'',5,5)
-insert into usuario_perfil values (1,GETDATE(),'',5,6)
-insert into usuario_perfil values (1,GETDATE(),'',5,7)
-insert into usuario_perfil values (1,GETDATE(),'',5,8)
-insert into usuario_perfil values (1,GETDATE(),'',2,9)
-insert into usuario_perfil values (1,GETDATE(),'',6,10)
-insert into usuario_perfil values (1,GETDATE(),'',7,11)
-insert into usuario_perfil values (1,GETDATE(),'',7,12)
-insert into usuario_perfil values (1,GETDATE(),'',8,13)
-
-
-
-
-INSERT [dbo].[dominio] ([nombre], [url], [base_dn],[manager_username],[manager_password], [filter_pattern], [activo]) VALUES ('EXACT','ldap://localhost:8389','dc=springframework,dc=org','','','uid=%s',1)
-
-INSERT [dbo].[tipo_autenticacion] ([nombre], [descripcion]) VALUES ('AUTENTICACION BASICA','INGRESO CON USUARIO Y CONTRASEÑA')
-INSERT [dbo].[tipo_autenticacion] ([nombre], [descripcion]) VALUES ('ACTIVE DIRECTORY', 'INGRESO CON DOMINIO')
-
-INSERT [dbo].[grupo_red] ([grupo_red_id],[nombre],[perfil_id]) VALUES (1,'OPERATIVO UTD',3)
-INSERT [dbo].[grupo_red] ([grupo_red_id],[nombre],[perfil_id]) VALUES (2,'USUARIO REGULAR',5)
-INSERT [dbo].[grupo_red] ([grupo_red_id],[nombre],[perfil_id]) VALUES (3,'USUARIO BLOQUE',7)
-INSERT [dbo].[grupo_red] ([grupo_red_id],[nombre],[perfil_id]) VALUES (4,'GESTION DOCUMENTAL',6)
-INSERT [dbo].[grupo_red] ([grupo_red_id],[nombre],[perfil_id]) VALUES (5,'SUPERVISOR UTD',2)
-
-
-
+USE [DB_GESTION_USUARIOS_PANEL]
+GO
+/****** Object:  Table [dbo].[tipo_autenticacion]    Script Date: 02/14/2020 09:58:55 ******/
+SET IDENTITY_INSERT [dbo].[tipo_autenticacion] ON
+INSERT [dbo].[tipo_autenticacion] ([id_tipo_autenticacion], [descripcion], [nombre]) VALUES (1, N'INGRESO CON USUARIO Y CONTRASEÃ‘A', N'AUTENTICACION BASICA')
+SET IDENTITY_INSERT [dbo].[tipo_autenticacion] OFF
+/****** Object:  Table [dbo].[usuarios]    Script Date: 02/14/2020 09:58:55 ******/
+SET IDENTITY_INSERT [dbo].[usuarios] ON
+INSERT [dbo].[usuarios] ([id_usuario], [activo], [apellido], [correo], [dni], [matricula], [nombre], [password], [username]) VALUES (1, 1, N'REYNOSO', N'preynoso@exact.com.pe', N'36252018', N'PREYNOSO', N'PAMELA', N'$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO', N'preynoso')
+INSERT [dbo].[usuarios] ([id_usuario], [activo], [apellido], [correo], [dni], [matricula], [nombre], [password], [username]) VALUES (2, 1, N'ADMINISTRADOR', N'administrador@exact.com.pe', N'36252017', N'ADMINSTRADOR', N'ADMINISTRADOR', N'$2a$04$95zHAMbf4jybXzbVSg0Xu.T1uEX.pgNpTNw1l539ntc4laRLvynYO', N'administrador')
+SET IDENTITY_INSERT [dbo].[usuarios] OFF
+/****** Object:  Table [dbo].[dominio]    Script Date: 02/14/2020 09:58:55 ******/
+/****** Object:  Table [dbo].[permisos]    Script Date: 02/14/2020 09:58:55 ******/
+SET IDENTITY_INSERT [dbo].[permisos] ON
+INSERT [dbo].[permisos] ([id_permiso], [descripcion], [nombre]) VALUES (1, N'USUARIO DEMO', N'ROLE_DEMO')
+INSERT [dbo].[permisos] ([id_permiso], [descripcion], [nombre]) VALUES (2, N'USUARIO MANTENIMIENTO', N'USER_MANTENIMIENTO')
+SET IDENTITY_INSERT [dbo].[permisos] OFF
+/****** Object:  Table [dbo].[perfiles]    Script Date: 02/14/2020 09:58:55 ******/
+SET IDENTITY_INSERT [dbo].[perfiles] ON
+INSERT [dbo].[perfiles] ([id_perfil], [activo], [descripcion], [f_creacion], [nombre]) VALUES (1, N'1', N'ES EL USUARIO QUE REALIZA PRESENTACIONES', CAST(0xBC400B00 AS Date), N'USUARIO_DEMO')
+INSERT [dbo].[perfiles] ([id_perfil], [activo], [descripcion], [f_creacion], [nombre]) VALUES (2, N'1', N'ES EL USUARIO QUE REALIZA PRESENTACIONES', CAST(0xBC400B00 AS Date), N'USER_MANTENIMIENTO')
+SET IDENTITY_INSERT [dbo].[perfiles] OFF
+/****** Object:  Table [dbo].[perfiles_permisos]    Script Date: 02/14/2020 09:58:56 ******/
+INSERT [dbo].[perfiles_permisos] ([perfil_id_perfil], [permisos_id_permiso]) VALUES (1, 1)
+INSERT [dbo].[perfiles_permisos] ([perfil_id_perfil], [permisos_id_permiso]) VALUES (2, 2)
+/****** Object:  Table [dbo].[grupo_red]    Script Date: 02/14/2020 09:58:56 ******/
+/****** Object:  Table [dbo].[usuario_perfil]    Script Date: 02/14/2020 09:58:56 ******/
+SET IDENTITY_INSERT [dbo].[usuario_perfil] ON
+INSERT [dbo].[usuario_perfil] ([id_usuario_perfil], [activo], [f_asignada], [f_baja], [perfil_id_perfil], [usuario_id_usuario]) VALUES (1, N'1', CAST(0xBC400B00 AS Date), CAST(0x5B950A00 AS Date), 1, 1)
+INSERT [dbo].[usuario_perfil] ([id_usuario_perfil], [activo], [f_asignada], [f_baja], [perfil_id_perfil], [usuario_id_usuario]) VALUES (2, N'1', CAST(0xBC400B00 AS Date), CAST(0x5B950A00 AS Date), 2, 2)
+SET IDENTITY_INSERT [dbo].[usuario_perfil] OFF
+/****** Object:  Table [dbo].[sesion]    Script Date: 02/14/2020 09:58:56 ******/
